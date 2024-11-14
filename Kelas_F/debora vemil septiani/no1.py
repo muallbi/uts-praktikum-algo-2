0e818794aaae7cce4e85064c504bb318a1c1ced8
muallbi <kleycrot@gmail.com>
2024-11-14 22:11:03 +0700

    Add files via upload

---
+++ Kelas_F/debora vemil septiani/no1.py
@@ -0,0 +1,9 @@
+# Program untuk menghitung luas lingkaran
+# Meminta input dari pengguna
+jari_jari = float(input("Masukkan jari-jari: "))
+# Menentukan nilai pi
+pi = 3.14
+# Menghitung luas lingkaran
+luas_lingkaran = pi * jari_jari ** 2
+# Menampilkan hasil
+print(f"Luas lingkaran: {luas_lingkaran:.2f}")
