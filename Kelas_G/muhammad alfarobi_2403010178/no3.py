293e06ac5371908ba1548c45e68f5477bdd4f730
muallbi <kleycrot@gmail.com>
2024-11-14 23:05:02 +0700

    Add files via upload

---
+++ Kelas_G/muhammad alfarobi_2403010178/no3.py
@@ -0,0 +1,5 @@
+kata_pertama = input("Masukkan kata pertama: ")
+kata_kedua = input("Masukkan kata kedua: ")
+kata_ketiga = input("Masukkan kata ketiga: ")
+kalimat = kata_pertama + " " + kata_kedua + " " + kata_ketiga
+print("Hasil penggabungan:", kalimat)
