9dfd42a8b4d1ab36dd522543fbc045007faee331
muallbi <kleycrot@gmail.com>
2024-11-14 22:29:29 +0700

    Delete muhammad alfarobi_2403010178 directory

--- muhammad alfarobi_2403010178/no4.py
+++
@@ -1,7 +0,0 @@
-kalimat = input("Masukkan kalimat: ")
-vokal = "aeiouAEIOU"
-jumlah_konsonan = 0
-for char in kalimat:
-    if char.isalpha() and char not in vokal:  
-        jumlah_konsonan += 1
-print("Jumlah huruf konsonan:", jumlah_konsonan)
